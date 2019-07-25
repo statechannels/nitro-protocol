6a618db095c4d68abde6280671e07650b41b7fd7
George Knee <georgeknee@googlemail.com>
2019-07-25 11:00:50 +0100

    ETHAH and ERC20AH inherit from AssetHolder

---
+++ contracts/AssetHolder.sol
@@ -0,0 +1,278 @@
+pragma solidity ^0.5.2;
+pragma experimental ABIEncoderV2;
+import "./Outcome.sol";
+import "./NitroLibrary.sol";
+contract AssetHolder {
+
+  
+   // TODO: Challenge duration should depend on the channel
+    uint constant CHALLENGE_DURATION = 5 minutes;
+
+    struct Authorization {
+        // Prevents replay attacks:
+        // It's required that the participant signs the message, meaning only
+        // the participant can authorize a withdrawal.
+        // Moreover, the participant should sign the address that they wish
+        // to send the transaction from, preventing any replay attack.
+        address participant; // the account used to sign commitment transitions
+        address destination; // either an account or a channel
+        uint amount;
+        address sender; // the account used to sign transactions
+    }
+
+    struct ConclusionProof {
+        Commitment.CommitmentStruct penultimateCommitment;
+        INitroLibrary.Signature penultimateSignature; 
+        Commitment.CommitmentStruct ultimateCommitment;
+        INitroLibrary.Signature ultimateSignature; 
+    }
+
+    mapping(address => uint) public holdings;
+    
+    // the outcomes in the Nitro network are spread over several AssetHolder contracts
+    // If they were in a single contract, we could store this information as:
+    // mapping(address => SingleAssetOutcome[]] ) outcomes;
+    // e.g. {
+    //     0xChannel1 => [
+    //      {
+    //         0xAssetHolder1,
+    //         [{0xAlice, 5}, {0XBob, 3}]
+    //     },
+    //     {
+    //         0xAssetHolder2,
+    //         [{0xAlice, 1}, {0XBob, 6}]
+    //      }]
+    // }
+    //   
+    // Since we are only concerned with a slice of this object here, we use
+    mapping(address => Outcome.allocation[]) public outcomes;
+    // TODO incorporate finalizesAt time
+
+    // **************
+    // Permissioned methods
+    // **************
+
+     function setOutcome(address channel, Outcome.allocation[] memory outcome) AdjudicatorOnly {
+        outcomes[channel] = outcome;
+    }
+
+    function setOutcome(address channel) AdjudicatorOnly {
+        outcomes[channel] = {};
+    }
+
+    // **************
+    // ETH and Token Management
+    // **************
+    
+    function transferAndWithdraw(address channel,
+        address participant,
+        address payable destination,
+        uint amount,
+        uint8 _v,
+        bytes32 _r,
+        bytes32 _s
+    ) public payable {
+        transfer(channel, participant, amount);
+        withdraw(participant, destination, amount, _v, _r ,_s);
+    }
+
+    function transfer(address channel, address destination, uint amount) public {
+        require(
+            outcomes[channel].challengeCommitment.guaranteedChannel == zeroAddress,
+            "Transfer: channel must be a ledger channel"
+        );
+        require(
+            outcomes[channel].finalizedAt <= now,
+            "Transfer: outcome must be final"
+        );
+        require(
+            outcomes[channel].finalizedAt > 0,
+            "Transfer: outcome must be present"
+        );
+
+        uint channelAffordsForDestination = Library.affords(destination, outcomes[channel], holdings[channel]);
+
+        require(
+            amount <= channelAffordsForDestination,
+            "Transfer: channel cannot afford the requested transfer amount"
+        );
+
+        holdings[destination] = holdings[destination] + amount;
+        holdings[channel] = holdings[channel] - amount;
+    }
+
+    // TODO this is a mix of asset management and outcome management
+    // TODO figure out if / how we still support this
+    // function concludeAndWithdraw(ConclusionProof memory proof,
+    //     address participant,
+    //     address payable destination,
+    //     uint amount,
+    //     address token,
+    //     uint8 _v,
+    //     bytes32 _r,
+    //     bytes32 _s
+    // ) public{
+    //     address channelId = proof.penultimateCommitment.channelId();
+    //     if (outcomes[channelId].finalizedAt > now || outcomes[channelId].finalizedAt == 0){
+    //     _conclude(proof);
+    //     } else {
+    //         require(keccak256(abi.encode(proof.penultimateCommitment)) == keccak256(abi.encode(outcomes[channelId].challengeCommitment)),
+    //         "concludeAndWithdraw: channel already concluded with a different proof");
+    //     }
+    //     transfer(channelId,participant, amount, token);
+    //     withdraw(participant,destination, amount, token, _v,_r,_s);
+    // }
+
+    function claim(address guarantor, address recipient, uint amount) public {
+        INitroLibrary.Outcome memory guarantee = outcomes[guarantor]; 
+        require(
+            guarantee.challengeCommitment.guaranteedChannel != zeroAddress,
+            "Claim: a guarantee channel is required"
+        );
+
+        require(
+            isChannelClosed(guarantor),
+            "Claim: channel must be closed"
+        );
+
+        uint funding = holdings[guarantor];
+        INitroLibrary.Outcome memory reprioritizedOutcome = Library.reprioritize( 
+            outcomes[guarantee.challengeCommitment.guaranteedChannel],
+            guarantee
+        );
+        if (Library.affords(recipient, reprioritizedOutcome, funding) >= amount) {
+            outcomes[guarantee.challengeCommitment.guaranteedChannel] = Library.reduce(
+                outcomes[guarantee.challengeCommitment.guaranteedChannel],
+                recipient,
+                amount,
+                token
+            );
+            holdings[guarantor] = holdings[guarantor].sub(amount);
+            holdings[recipient] = holdings[recipient].add(amount);
+        } else {
+            revert('Claim: guarantor must be sufficiently funded');
+        }
+    }
+
+    function reprioritize(
+        Outcome memory allocation,
+        Outcome memory guarantee
+    ) public pure returns (Outcome memory) {
+        require(
+            guarantee.challengeCommitment.guaranteedChannel != zeroAddress,
+            "Claim: a guarantee channel is required"
+        );
+        address[] memory newDestination = new address[](guarantee.destination.length);
+        uint[] memory newAllocation = new uint[](guarantee.destination.length);
+        for (uint aIdx = 0; aIdx < allocation.destination.length; aIdx++) {
+            for (uint gIdx = 0; gIdx < guarantee.destination.length; gIdx++) {
+                if (guarantee.destination[gIdx] == allocation.destination[aIdx]) {
+                    newDestination[gIdx] = allocation.destination[aIdx];
+                    newAllocation[gIdx] = allocation.allocation[aIdx];
+                    break;
+                }
+            }
+        }
+
+        return Outcome(
+            newDestination,
+            allocation.finalizedAt,
+            allocation.challengeCommitment,
+            newAllocation,
+            allocation.token
+        );
+    }
+
+    function affords(
+        address recipient,
+        Outcome memory outcome,
+        uint funding
+    ) public pure returns (uint256) {
+        uint result = 0;
+        uint remainingFunding = funding;
+
+        for (uint i = 0; i < outcome.destination.length; i++) {
+            if (remainingFunding <= 0) {
+                break;
+            }
+
+            if (outcome.destination[i] == recipient) {
+                // It is technically allowed for a recipient to be listed in the
+                // outcome multiple times, so we must iterate through the entire
+                // array.
+                result = result.add(min(outcome.allocation[i], remainingFunding));
+            }
+            if (remainingFunding > outcome.allocation[i]){
+                remainingFunding = remainingFunding.sub(outcome.allocation[i]);
+            }else{
+                remainingFunding = 0;
+            }
+        }
+
+        return result;
+    }
+
+    function reduce(
+        Outcome memory outcome,
+        address recipient,
+        uint amount,
+        address token
+    ) public pure returns (Outcome memory) {
+        // TODO only reduce entries corresponding to token argument
+        uint256[] memory updatedAllocation = outcome.allocation;
+        uint256 reduction = 0;
+        uint remainingAmount = amount;
+        for (uint i = 0; i < outcome.destination.length; i++) {
+            if (outcome.destination[i] == recipient) {
+                // It is technically allowed for a recipient to be listed in the
+                // outcome multiple times, so we must iterate through the entire
+                // array.
+                reduction = reduction.add(min(outcome.allocation[i], remainingAmount));
+                remainingAmount = remainingAmount.sub(reduction);
+                updatedAllocation[i] = updatedAllocation[i].sub(reduction);
+            }
+        }
+
+        return Outcome(
+            outcome.destination,
+            outcome.finalizedAt,
+            outcome.challengeCommitment, // Once the outcome is finalized,
+            updatedAllocation,
+            outcome.token
+        );
+    }
+
+    function moveAuthorized(Commitment.CommitmentStruct memory _commitment, Signature memory signature) public pure returns (bool){
+        return _commitment.mover() == recoverSigner(
+            abi.encode(_commitment),
+            signature.v,
+            signature.r,
+            signature.s
+        );
+    }
+
+    function min(uint a, uint b) public pure returns (uint) {
+        if (a <= b) {
+            return a;
+        }
+
+        return b;
+    }
+
+    function recoverSigner(bytes memory _d, uint8 _v, bytes32 _r, bytes32 _s) public pure returns(address) {
+    bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+    bytes32 h = keccak256(_d);
+
+    bytes32 prefixedHash = keccak256(abi.encodePacked(prefix, h));
+
+    address a = ecrecover(prefixedHash, _v, _r, _s);
+
+    return(a);
+    }
+
+    // ****************
+    // Events
+    // ****************
+    event Deposited(address destination, uint256 amountDeposited, uint256 destinationHoldings);
+
+}
--- contracts/ERC20AssetHolder.sol
+++ contracts/ERC20AssetHolder.sol
@@ -2,7 +2,9 @@ pragma solidity ^0.5.2;
 pragma experimental ABIEncoderV2;
 import "./Outcome.sol";
 import "./NitroLibrary.sol";
-contract AssetHolder {
+import "./AssetHolder.sol";
+
+contract ERC20AssetHolder is AssetHolder {
 
     constructor(address _AdjudicatorAddress, address _TokenAddress) public {
         address AdjudicatorAddress = _AdjudicatorAddress;
@@ -15,61 +17,6 @@ contract AssetHolder {
     }
 
     IERC20 _token = IERC20(TokenAddress);
-
-    // TODO: Challenge duration should depend on the channel
-    uint constant CHALLENGE_DURATION = 5 minutes;
-
-    struct Authorization {
-        // Prevents replay attacks:
-        // It's required that the participant signs the message, meaning only
-        // the participant can authorize a withdrawal.
-        // Moreover, the participant should sign the address that they wish
-        // to send the transaction from, preventing any replay attack.
-        address participant; // the account used to sign commitment transitions
-        address destination; // either an account or a channel
-        uint amount;
-        address sender; // the account used to sign transactions
-    }
-
-    struct ConclusionProof {
-        Commitment.CommitmentStruct penultimateCommitment;
-        INitroLibrary.Signature penultimateSignature; 
-        Commitment.CommitmentStruct ultimateCommitment;
-        INitroLibrary.Signature ultimateSignature; 
-    }
-
-    mapping(address => uint) public holdings;
-    
-    // the outcomes in the Nitro network are spread over several AssetHolder contracts
-    // If they were in a single contract, we could store this information as:
-    // mapping(address => Outcome.sSingleAssetOutcome[] ) outcomes;
-    // e.g. {
-    //     0xChannel1 => [
-    //      {
-    //         0xAssetHolder1,
-    //         [{0xAlice, 5}, {0XBob, 3}]
-    //     },
-    //     {
-    //         0xAssetHolder2,
-    //         [{0xAlice, 1}, {0XBob, 6}]
-    //      }]
-    // }
-    //   
-    // Since we are only concerned with a slice of this object here, we use
-    mapping(address => Outcome.allocation[]) outcomes;
-    // TODO incorporate finalizesAt time
-
-    // **************
-    // Permissioned methods
-    // **************
-
-     function setOutcome(address channel, Outcome.allocation[] memory outcome) AdjudicatorOnly {
-        outcomes[channel] = outcome;
-    }
-
-    function setOutcome(address channel) AdjudicatorOnly {
-        outcomes[channel] = {};
-    }
 
     // **************
     // Asset Management
@@ -108,18 +55,6 @@ function deposit(address destination, uint expectedHeld,
         emit Deposited(destination, amountDeposited, holdings[destination]);
     }
 
-    function transferAndWithdraw(address channel,
-        address participant,
-        address payable destination,
-        uint amount,
-        uint8 _v,
-        bytes32 _r,
-        bytes32 _s
-    ) public payable {
-        transfer(channel, participant, amount);
-        withdraw(participant, destination, amount, _v, _r ,_s);
-    }
-
     function withdraw(address participant,
         address payable destination,
         uint amount,
@@ -144,91 +79,8 @@ function deposit(address destination, uint expectedHeld,
         );
 
         holdings[participant] = holdings[participant].sub(amount);
-        // Decrease holdings before calling to token contract (protect against reentrancy)
+        // Decrease holdings before calling transfer (protect against reentrancy)
         _token.transfer(destination,amount);
     }
-
-
-    function transfer(address channel, address destination, uint amount) public {
-        require(
-            outcomes[channel].challengeCommitment.guaranteedChannel == zeroAddress,
-            "Transfer: channel must be a ledger channel"
-        );
-        require(
-            outcomes[channel].finalizedAt <= now,
-            "Transfer: outcome must be final"
-        );
-        require(
-            outcomes[channel].finalizedAt > 0,
-            "Transfer: outcome must be present"
-        );
-
-        uint channelAffordsForDestination = Library.affords(destination, outcomes[channel], holdings[channel]);
-
-        require(
-            amount <= channelAffordsForDestination,
-            "Transfer: channel cannot afford the requested transfer amount"
-        );
-
-        holdings[destination] = holdings[destination] + amount;
-        holdings[channel] = holdings[channel] - amount;
-    }
-
-    // TODO this is a mix of asset management and outcome management
-    // TODO figure out if / how we still support this
-    // function concludeAndWithdraw(ConclusionProof memory proof,
-    //     address participant,
-    //     address payable destination,
-    //     uint amount,
-    //     address token,
-    //     uint8 _v,
-    //     bytes32 _r,
-    //     bytes32 _s
-    // ) public{
-    //     address channelId = proof.penultimateCommitment.channelId();
-    //     if (outcomes[channelId].finalizedAt > now || outcomes[channelId].finalizedAt == 0){
-    //     _conclude(proof);
-    //     } else {
-    //         require(keccak256(abi.encode(proof.penultimateCommitment)) == keccak256(abi.encode(outcomes[channelId].challengeCommitment)),
-    //         "concludeAndWithdraw: channel already concluded with a different proof");
-    //     }
-    //     transfer(channelId,participant, amount, token);
-    //     withdraw(participant,destination, amount, token, _v,_r,_s);
-    // }
-
-    function claim(address guarantor, address recipient, uint amount) public {
-        INitroLibrary.Outcome memory guarantee = outcomes[guarantor]; 
-        require(
-            guarantee.challengeCommitment.guaranteedChannel != zeroAddress,
-            "Claim: a guarantee channel is required"
-        );
-
-        require(
-            isChannelClosed(guarantor),
-            "Claim: channel must be closed"
-        );
-
-        uint funding = holdings[guarantor];
-        INitroLibrary.Outcome memory reprioritizedOutcome = Library.reprioritize( 
-            outcomes[guarantee.challengeCommitment.guaranteedChannel],
-            guarantee
-        );
-        if (Library.affords(recipient, reprioritizedOutcome, funding) >= amount) {
-            outcomes[guarantee.challengeCommitment.guaranteedChannel] = Library.reduce(
-                outcomes[guarantee.challengeCommitment.guaranteedChannel],
-                recipient,
-                amount
-            );
-            holdings[guarantor] = holdings[guarantor].sub(amount);
-            holdings[recipient] = holdings[recipient].add(amount);
-        } else {
-            revert('Claim: guarantor must be sufficiently funded');
-        }
-    }
-
-    // ****************
-    // Events
-    // ****************
-    event Deposited(address destination, uint256 amountDeposited, uint256 destinationHoldings);
 
 }
